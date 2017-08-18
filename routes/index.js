04b391b1ebac8f8ff2cfe20f815712f5d0230db2
Joseph Gordy <jgordy2424@gmail.com>
2017-08-17 21:03:27 -0400

    All basic functionality complete with basic styling

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router  = express.Router();
 
 let user = { username: "1tacocat1", password: "imApalindrome"};
+let messages = [];
 
 // root endpoint
 router.get("/", function(req,res) {
   if (req.session.token) {
-    res.render("results");
+    let obj = {username: user.username}
+    res.render("results", obj);
   } else {
     res.redirect("/login");
   }
@@ -14,13 +16,8 @@ router.get("/", function(req,res) {
 
 // login endpoint
 router.get("/login", function(req, res) {
-
-  req.checkBody("username", "Username cannot be empty").notEmpty();
-  req.checkBody("username", "Username must contain between 8 and 25 characters").len(8, 25);
-  req.checkBody("password", "Password cannot be empty").notEmpty();
-  req.checkBody("password", "Password must contain between 8 and 25 characters").len(8, 25);
-  
- res.render("login");
+ res.render("login", {messages});
+ messages = [];
 });
 
 // upon submiting
@@ -29,23 +26,46 @@ router.post("/results", function(req, res) {
     username: req.body.username,
     password: req.body.password
   };
+  messages = [];
 
-  if (obj.username == user.username && obj.password == user.password) {
-    req.session.user = obj;
-    req.session.token = "afs29628";
-    res.render("results", obj);
-  } else {
-    res.redirect("/");
-  }
-});
+  req.checkBody("username", "Username cannot be empty!").notEmpty();
+  req.checkBody("username", "Username must contain between 8 and 25 characters!").len(8, 25);
+  // req.checkBody("username", "Username cannot contain special characters. Ex: @!*").isAlphanumeric("en-US")
+  req.checkBody("password", "Password cannot be empty!").notEmpty();
+  req.checkBody("password", "Password must contain between 8 and 25 characters!").len(8, 25);
+  // req.checkBody("Password", "Password cannot contain special characters. Ex: @!*").isAlphanumeric("en-US")
+
+  let errors = req.getValidationResult();
+  // let messages = [];
+
+  errors.then(function(result) {
+    result.array().forEach(function(error) {
+      messages.push(error.msg);
+    });
+
+    let errObject = {
+      errors: messages,
+      info: req.body
+    };
+
+      if (obj.username == user.username && obj.password == user.password) {
+        req.session.user = obj;
+        req.session.token = "afs29628";
+        res.render("results", obj);
+      } else if (messages !== []) {
+      console.log("why?????");
+      res.redirect("/",)
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
 
 //  logout button
 router.get("/logout", function(req, res) {
-  // req.session.destroy(); is good too
   req.session.destroy(function(err) {
     console.log(err);
   });
-
   res.redirect("/");
 });
 
